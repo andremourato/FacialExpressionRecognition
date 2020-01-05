bbddffbe7af51a69b8daa74c7346cbe8fc651c96
André Mourato <andremourato@ua.pt>
2020-01-05 05:52:06 +0000

    Added support for CUDA and tested in a GTX1060

--- src/facialrecognition.py
+++ src/facialrecognition.py
@@ -1,5 +1,5 @@
 import cv2
-import label_image
+from label_image import *
 
 size = 4
 
@@ -9,34 +9,40 @@ classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 webcam = cv2.VideoCapture(0) #Using default WebCam connected to the PC.
 
-while True:
-    (rval, im) = webcam.read()
-    im=cv2.flip(im,1,0) #Flip to act as a mirror
+graph = load_graph("retrained_graph.pb")
 
-    # Resize the image to speed up detection
-    mini = cv2.resize(im, (int(im.shape[1]/size), int(im.shape[0]/size)))
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.65)
 
-    # detect MultiScale / faces 
-    faces = classifier.detectMultiScale(mini)
-    # Draw rectangles around each face
-    for f in faces:
-        (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
-        cv2.rectangle(im, (x,y), (x+w,y+h), (0,255,0), 4)
-        
-        #Save just the rectangle faces in SubRecFaces
-        zoomed_Face = im[y:y+h, x:x+w]
+with tf.Session(graph=graph,
+    config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    while True:
+        (rval, im) = webcam.read()
+        im=cv2.flip(im,1,0) #Flip to act as a mirror
 
-        CurrentFaceFile = "test.jpg" #Saving the current image from the webcam for testing.
-        cv2.imwrite(CurrentFaceFile, zoomed_Face)
-        
-        text = label_image.main(CurrentFaceFile)# Gets the corresponding label (angry,happy,etc.)
-        text = text.title()
-        font = cv2.FONT_HERSHEY_TRIPLEX
-        cv2.putText(im, text,(x+w,y), font, 1, (0,0,255), 2)
+        # Resize the image to speed up detection
+        mini = cv2.resize(im, (int(im.shape[1]/size), int(im.shape[0]/size)))
 
-    # Show the image
-    cv2.imshow('Capture',   im)
-    key = cv2.waitKey(10)
-    # if Esc key is press then break out of the loop 
-    if key == 27: #The Esc key
-        break
+        # detect MultiScale / faces 
+        faces = classifier.detectMultiScale(mini)
+        # Draw rectangles around each face
+        for f in faces:
+            (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
+            cv2.rectangle(im, (x,y), (x+w,y+h), (0,255,0), 4)
+            
+            #Save just the rectangle faces in SubRecFaces
+            zoomed_Face = im[y:y+h, x:x+w]
+
+            CurrentFaceFile = "test.jpg" #Saving the current image from the webcam for testing.
+            cv2.imwrite(CurrentFaceFile, zoomed_Face)
+            
+            text = main(CurrentFaceFile,graph,sess)# Gets the corresponding label (angry,happy,etc.)
+            text = text.title()
+            font = cv2.FONT_HERSHEY_TRIPLEX
+            cv2.putText(im, text,(x+w,y), font, 1, (0,0,255), 2)
+
+        # Show the image
+        cv2.imshow('Capture',   im)
+        key = cv2.waitKey(10)
+        # if Esc key is press then break out of the loop 
+        if key == 27: #The Esc key
+            break
--- src/label_image.py
+++ src/label_image.py
@@ -52,7 +52,7 @@ def load_labels(label_file):
     label.append(l.rstrip())
   return label
 
-def main(img):
+def main(img,graph,sess):
   file_name = img
   model_file = "retrained_graph.pb"
   label_file = "retrained_labels.txt"
@@ -63,38 +63,6 @@ def main(img):
   input_layer = "input"
   output_layer = "final_result"
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--image", help="image to be processed")
-  parser.add_argument("--graph", help="graph/model to be executed")
-  parser.add_argument("--labels", help="name of file containing labels")
-  parser.add_argument("--input_height", type=int, help="input height")
-  parser.add_argument("--input_width", type=int, help="input width")
-  parser.add_argument("--input_mean", type=int, help="input mean")
-  parser.add_argument("--input_std", type=int, help="input std")
-  parser.add_argument("--input_layer", help="name of input layer")
-  parser.add_argument("--output_layer", help="name of output layer")
-  args = parser.parse_args()
-
-  if args.graph:
-    model_file = args.graph
-  if args.image:
-    file_name = args.image
-  if args.labels:
-    label_file = args.labels
-  if args.input_height:
-    input_height = args.input_height
-  if args.input_width:
-    input_width = args.input_width
-  if args.input_mean:
-    input_mean = args.input_mean
-  if args.input_std:
-    input_std = args.input_std
-  if args.input_layer:
-    input_layer = args.input_layer
-  if args.output_layer:
-    output_layer = args.output_layer
-
-  graph = load_graph(model_file)
   t = read_tensor_from_image_file(file_name,
                                   input_height=input_height,
                                   input_width=input_width,
@@ -107,11 +75,10 @@ def main(img):
   output_operation = graph.get_operation_by_name(output_name);
 
 
-  with tf.Session(graph=graph) as sess:
-    start = time.time()
-    results = sess.run(output_operation.outputs[0],
-                      {input_operation.outputs[0]: t})
-    end=time.time()
+  start = time.time()
+  results = sess.run(output_operation.outputs[0],
+                    {input_operation.outputs[0]: t})
+  end=time.time()
   results = np.squeeze(results)
 
   top_k = results.argsort()[-5:][::-1]
